1bc3980f386002a0e866a441af28cc2dacd17cc6
Azad Ates <azad.ates@protonmail.com>
2023-01-10 16:53:13 +0100

    refactor: modularize task functionality and improve reactivity

---
+++ src/components/AddToDo.vue
@@ -0,0 +1,87 @@
+<script setup>
+    import { ref } from 'vue';
+    const props = defineProps({
+        tasks: Array,
+    })
+
+    let newTask = ref("")
+    function addToDo() {
+        props.tasks.push({task: newTask.value, complete:false});
+        newTask.value = ""
+    }
+</script>
+
+<template>
+    <form onsubmit="return false">
+      <div class="newTask">
+        <div class="checkboxWrapper">
+          <input type="checkbox" disabled />
+        </div>
+        <input type="submit" hidden @click="addToDo">
+        <input type="text" placeholder="Add new Task" v-model="newTask">
+      </div>
+      <div class="footer">
+        <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+          <path fill="currentColor" d="M7 11H9V13H7V11M19 3H18V1H16V3H8V1H6V3H5C3.9 3 3 3.9 3 5V19C3 20.1 3.9 21 5 21H19C20.1 21 21 20.1 21 19V5C21 3.9 20.1 3 19 3M19 5V7H5V5H19M5 19V9H19V19H5M11 15H13V17H11V15M15 15H17V17H15V15M15 11H17V13H15V11Z" />
+        </svg>
+        <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+          <path fill="currentColor" d="M10 21H14C14 22.1 13.1 23 12 23S10 22.1 10 21M21 19V20H3V19L5 17V11C5 7.9 7 5.2 10 4.3V4C10 2.9 10.9 2 12 2S14 2.9 14 4V4.3C17 5.2 19 7.9 19 11V17L21 19M17 11C17 8.2 14.8 6 12 6S7 8.2 7 11V18H17V11Z" />
+        </svg>
+        <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+          <path fill="currentColor" d="M17,17H7V14L3,18L7,22V19H19V13H17M7,7H17V10L21,6L17,2V5H5V11H7V7Z" />
+        </svg>
+        <input type="submit" value="add" @click="addToDo" />
+      </div>
+    </form>
+</template>
+
+<style>
+  .newTask  {
+    display: flex;
+    align-items: center;
+    height: 45px;
+    background-color: white;
+    border: 1px solid lightgrey;
+    border-radius: 5px;
+  }
+  .checkboxWrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 45px;
+  }
+
+  .newTask input[type=text] {
+    flex: 1;
+    border-style: hidden;
+    color: #2564cf;
+  }
+
+  .newTask input[type=text]:focus {
+    outline: none;
+    color: black;
+    border-style: hidden;
+  }
+
+  .footer {
+    display: flex;
+    align-items: center;
+    height: 38px;
+    border-radius: 5px;
+    background-color: #faf9f8;
+    border: 1px solid lightgrey;
+  }
+
+  .footer > * {
+    padding: 5px;
+    margin: 5px
+  }
+  
+  .footer input[type=submit] {
+    margin-left: auto;
+  }
+
+  .footer svg {
+    color: gray;
+  }
+</style>
--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,31 +1,72 @@
 <script setup>
+    import { ref } from 'vue';
+
     const props = defineProps({
         tasks: Array,
+        canToggle: Boolean = false
     })
 
-    function alertToDo(toDo) {
-        alert('task:\t\t' + toDo.task + '\n' + 'complete:\t' + toDo.complete)
-    }
+    let visible = ref(!props.canToggle)
+    let toggleVisibility = () => visible.value = !visible.value
+    let alertTask = (task) => alert('task:\t\t' + task.task + '\n' + 'complete:\t' + task.complete)
 </script>
 
 <template>
-    <ul class="toDoList">
-        <li class="toDoListItem" v-for="toDo in props.tasks">
-            <label class="checkboxWrapper">
-                <input type="checkbox" v-model="toDo.complete" />
-            </label>
-            <p @click="alertToDo(toDo)">
-                {{toDo.task}}
-            </p>
-            <svg class="toDoListItem" style="width:24px;height:24px;" viewBox="0 0 24 24">
-                <path fill="currentColor" d="M12,15.39L8.24,17.66L9.23,13.38L5.91,10.5L10.29,10.13L12,6.09L13.71,10.13L18.09,10.5L14.77,13.38L15.76,17.66M22,9.24L14.81,8.63L12,2L9.19,8.63L2,9.24L7.45,13.97L5.82,21L12,17.27L18.18,21L16.54,13.97L22,9.24Z" />
-            </svg>
-        </li>
-    </ul>
+    <div v-if="props.tasks.length">
+        <div class="title" v-if="canToggle" :class="visible ? '' : 'toggled'">
+            <button @click="toggleVisibility">
+                <p><slot></slot></p>
+                <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                    <path fill="currentColor" d="M8 9H16L12 16" />
+                </svg>
+            </button>
+        </div>
+        <div class="title" v-else>
+            <p><slot></slot></p>
+        </div>
+
+        <ul class="taskList" v-if="visible">
+            <li class="taskListItem" v-for="task in props.tasks" :key="props.tasks">
+                <label class="checkboxWrapper">
+                    <input type="checkbox" v-model="task.complete" />
+                </label>
+                <p @click="alertTask(task)">
+                    {{task.task}}
+                </p>
+                <svg class="taskListItem" style="width:24px;height:24px;" viewBox="0 0 24 24">
+                    <path fill="currentColor" d="M12,15.39L8.24,17.66L9.23,13.38L5.91,10.5L10.29,10.13L12,6.09L13.71,10.13L18.09,10.5L14.77,13.38L15.76,17.66M22,9.24L14.81,8.63L12,2L9.19,8.63L2,9.24L7.45,13.97L5.82,21L12,17.27L18.18,21L16.54,13.97L22,9.24Z" />
+                </svg>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <style scoped>
-    .toDoList {
+    .toggled {
+        top: -20px !important;
+    }
+
+    .title {    
+        display: flex;
+        align-items: center;
+        position: relative;
+        left: 5px;
+        top: 20px;
+
+    }
+
+    .title button {
+        display: flex;
+        align-items: center;
+        padding: 0;
+        margin: 0;
+        border: none;
+        background: none;
+    }
+    .title p:empty{
+        display: none;
+    }
+    .taskList {
         display: flex;
         flex-direction: column;
         gap: 5px;
@@ -33,7 +74,7 @@
         padding-left: 0;
     }
 
-    .toDoListItem  {
+    .taskListItem  {
         display: flex;
         align-items: center;
         height: 45px;
@@ -42,7 +83,7 @@
         border: 1px solid lightgrey;
     }
 
-    .toDoListItem:hover, .checkboxWrapper:hover, .checkboxWrapper input:hover {
+    .taskListItem:hover, .checkboxWrapper:hover, .checkboxWrapper input:hover {
         cursor: pointer;
     }
 
@@ -53,11 +94,11 @@
         width: 45px;
     }
 
-    .toDoListItem p {
+    .taskListItem p {
         flex: 1;
     }
 
-    .toDoListItem svg {
+    .taskListItem svg {
         margin-left: auto;
         margin-right: 10px;
         border: none;
